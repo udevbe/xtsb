f02634608c30cd635aa1e3c7d60c56b3d08f3ad7
Erik De Rijcke <derijcke.erik@gmail.com>
2023-01-14 21:03:09 +0100

    don't depend on websocket interface

--- src/webConnection.ts
+++ src/webConnection.ts
@@ -1,6 +1,18 @@
 import { SetupConnection, XConnectionSocket } from './connection'
 import { Setup } from './xcb'
 
+const textDecoder = new TextDecoder()
+
+export interface Channel {
+  onError(cb: (ev: Event) => void): void
+
+  onMessage(cb: (ev: Uint8Array) => void): void
+
+  close(): void
+
+  send(data: Uint8Array): void
+}
+
 function isSetup(setup: any): setup is Setup {
   // TODO we could check all Setup attributes but it would be a long list...
   return (
@@ -11,44 +23,35 @@ function isSetup(setup: any): setup is Setup {
   )
 }
 
-function createXConnectionSocket(webSocket: WebSocket): XConnectionSocket {
-  webSocket.binaryType = 'arraybuffer'
-  const xConnectionSocket: XConnectionSocket = {
-    close() {
-      webSocket.close()
-    },
-    write(data: Uint8Array) {
-      webSocket.send(data)
-    },
-  }
-  webSocket.onmessage = (ev: MessageEvent<ArrayBuffer>) =>
-    xConnectionSocket.onData?.(new Uint8Array(ev.data, 0, ev.data.byteLength))
-  webSocket.onerror = (ev) => {
-    xConnectionSocket.close()
-    console.error('XConnection is in error: ' + ev)
-  }
-  return xConnectionSocket
-}
-
-async function auth(webSocket: WebSocket) {
-  return new Promise<Setup>((resolve, reject) => {
-    webSocket.onmessage = (ev) => {
-      const messageData = typeof ev.data === 'string' ? ev.data : new TextDecoder().decode(ev.data)
-      const message = JSON.parse(messageData)
-      if (isSetup(message)) {
-        resolve(message)
+export const webConnectionSetup: (channel: Channel) => SetupConnection = (channel) => async () => {
+  return new Promise<{ setup: Setup; xConnectionSocket: XConnectionSocket }>((resolve, reject) => {
+    let xConnectionSocket: XConnectionSocket | undefined
+    channel.onMessage((ev) => {
+      if (xConnectionSocket) {
+        xConnectionSocket.onData?.(ev)
       } else {
-        reject('Expected xcb Setup, got: ' + ev)
-      }
-    }
-    webSocket.onerror = (ev) => {
-      reject('WebSocket is in error: ' + ev)
-    }
-  })
-}
+        const messageData = textDecoder.decode(ev)
+        const message = JSON.parse(messageData)
+        if (isSetup(message)) {
+          const xConnection = {
+            close() {
+              channel.close()
+            },
+            write(data: Uint8Array) {
+              channel.send(data)
+            },
+          }
+          xConnectionSocket = xConnection
+          channel.onError((ev) => {
+            xConnection.close()
+            console.error('XConnection is in error: ' + ev)
+          })
 
-export const webConnectionSetup: (webSocket: WebSocket) => SetupConnection = (webSocket) => async () => {
-  const setup = await auth(webSocket)
-  const xConnectionSocket = createXConnectionSocket(webSocket)
-  return { setup, xConnectionSocket }
+          resolve({ setup: message, xConnectionSocket })
+        } else {
+          reject('Expected xcb Setup, got: ' + ev)
+        }
+      }
+    })
+  })
 }
